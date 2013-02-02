bda9e650d992967d5e076cf815326c3f22cbf678
Alê Borba <alexandre.borba@imasters.com.br>
2013-02-02 19:46:46 -0200

    Retirada dos arquivos de configuração especificos da IDE e inserção no .gitignore

--- teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo  "<table border=1>";
-echo  "<tr><td rowspan=2></td><td></td><td></td><td></td><td></td></tr>";
-echo  "<tr><td></td><td></td><td></td><td></td><td></td></tr>";
-echo  "<tr><td></td><td></td><td></td><td></td><td></td></tr>";
-echo  "</table>";
-
-?>
